7acb675d2b33e2781277f4a31eb6a923243194e6
3NYAM3 <09jjrr21@naver.com>
2022-11-25 15:29:17 +0900

    알람 최저/최고 기준치 변수 이름 변경

--- app/src/main/java/com/example/smartfactory/network/DTO/AlarmDTO.java
+++ app/src/main/java/com/example/smartfactory/network/DTO/AlarmDTO.java
@@ -7,16 +7,16 @@ public class AlarmDTO {
     Long index; // 알람 구분 인덱스
     Long sensorIndex; // 센서 고유 인덱스
     // on/off알람일경우 ?
-    double from; // 알람 최저 기준치
-    double to; // 알람 최고 기준치
+    double minimum; // 알람 최저 기준치
+    double maximum; // 알람 최고 기준치
     private LocalDateTime createdDate;
     private LocalDateTime modifiedDate;
 
-    public AlarmDTO(Long index, Long sensorIndex, double from, double to, LocalDateTime createdDate, LocalDateTime modifiedDate) {
+    public AlarmDTO(Long index, Long sensorIndex, double minimum, double maximum, LocalDateTime createdDate, LocalDateTime modifiedDate) {
         this.index = index;
         this.sensorIndex = sensorIndex;
-        this.from = from;
-        this.to = to;
+        this.minimum = minimum;
+        this.maximum = maximum;
         this.createdDate = createdDate;
         this.modifiedDate = modifiedDate;
     }
